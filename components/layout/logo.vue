38f8418f286f8c3afe7d13935970ae4392040eea
Anne Brown <anne@browndomain.com>
2024-11-13 09:37:52 -0500

    Yarn, npm Barnacles

--- components/layout/logo.vue
+++ components/layout/logo.vue
@@ -3,24 +3,22 @@
 		id="header__logo"
 		class="company-logo"
 	>
-    <NuxtLink to="https://annebrown.ca" target="_blank">
+        <NuxtLink to="https://annebrown.ca" target="_blank">
 			<img
 				src="/images/logos/book.svg"
 				alt="logo"
-				width="300"
-				height="300"
+				width="200"
+				height="200"
 			/>
 		</NuxtLink>	
-    <NuxtLink to="https://misscommunication.ca" target="_blank">
+        <NuxtLink to="https://misscommunication.ca" target="_blank">
 			<img
 				src="/images/logos/logo.svg"
 				alt="logo"
-				width="300"
-				height="300"
+				width="200"
+				height="200"
 			/>
-		</NuxtLink>
-       
-		<span>cms.misscommunication.ca</span>
+	    </NuxtLink>
 	</div>
 </template>
 
@@ -32,22 +30,11 @@
 		padding-bottom: 3em;
 		padding-right: 1em;
 		width: max-content;
-		border-right: 1px solid $light-grey;
 		img {
 			position: relative;
-			height: 2em;
+			height: 3em;
 			width: auto;
 		}
-		span {
-			position: absolute;
-			font-size: 0.8em;
-			right: 1em;
-			bottom: 0;
-            width: max-content;
-			transform: translateX(5%);
-			font-style: italic;
-			font-family: $font-art;
-			color: $dark-grey;
-		}
+
 	}
 </style>
